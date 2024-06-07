d974e881e4f5a24d641014391856acb9c910a75a
Neyleth Quintero <neylethquintero1997@gmail.com>
2024-06-06 22:10:00 -0300

    feat: ready hack

--- hack_10.py
+++ hack_10.py
@@ -3,6 +3,23 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 """
 
 def fn_hack_10():
-    result = "fooziman"
-    #...
-    return result  
+    result = []
+    text = "fooziman"
+    for char in text:
+        if char == "o":
+            result.append("0")
+        elif char == "i":
+            result.append("1")
+        elif char == "a":
+            result.append("@")
+        elif char == "f":
+            result.append("F")
+        elif char == "z":
+            result.append("Z")
+        elif char == "m":
+            result.append("M")
+        elif char == "n":
+            result.append("N")
+        else:
+            result.append(char)
+    return result
--- hack_9.py
+++ hack_9.py
@@ -4,5 +4,8 @@ loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
 
 def fn_hack_9():
     result = [1,2,3]
-    #...
-    return result  
+    nuevo = 1
+    while nuevo < len(result):
+        result.insert(nuevo,'@')
+        nuevo += 2
+    return result
